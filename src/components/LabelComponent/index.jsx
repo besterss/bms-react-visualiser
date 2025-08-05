fbdedf12f6514632674ddf4cd964c4f89b1c58e9
Suchanek Mariusz <mariusz.suchanek7@gmail.com>
2025-08-05 13:41:44 +0200

    small update for optimalization

--- src/components/LabelComponent/index.jsx
+++ src/components/LabelComponent/index.jsx
@@ -9,22 +9,18 @@ const LabelComponent = ({
   text,
   floorIndex,
   config,
-}) =>
+}) => {
   useEffect(() => {
     if (!scene || typeof floorIndex !== "number" || !config) return;
 
-    const wall_height = config.visualization.wall_height;
-    const floor_thickness = config.visualization.floor_thickness;
-    const floor_spacing = config.visualization.floor_spacing;
-    const room_floor_height = config.visualization.room_floor_height;
-
+    const { wall_height, floor_thickness, floor_spacing, room_floor_height } =
+      config.visualization;
     const yOffset = wall_height + floor_thickness + floor_spacing;
     const yPosition = Math.max(
       0,
       floorIndex * yOffset + room_floor_height + 0.2
     );
 
-    // Nastav rozlišení textury a poměr
     const textureWidth = 1024;
     const textureHeight = 256;
     const dynamicTexture = new BABYLON.DynamicTexture(
@@ -33,14 +29,20 @@ const LabelComponent = ({
       scene
     );
     dynamicTexture.hasAlpha = true;
-    dynamicTexture.drawText(text, null, 140, "600 46px Roboto", true);
+    dynamicTexture.drawText(
+      text,
+      null,
+      140,
+      "600 46px Roboto",
+      "black",
+      "transparent"
+    );
 
     const material = new BABYLON.StandardMaterial("TextMaterial", scene);
     material.diffuseTexture = dynamicTexture;
-    material.emissiveColor = new BABYLON.Color3(1, 1, 1);
-    material.specularColor = new BABYLON.Color3(0, 0, 0);
+    material.emissiveColor = BABYLON.Color3.White();
+    material.specularColor = BABYLON.Color3.Black();
 
-    // Vytvořte rovinu s odpovídajícími rozměry
     const planeWidth = 12;
     const planeHeight = planeWidth * (textureHeight / textureWidth);
     const plane = BABYLON.MeshBuilder.CreatePlane(
@@ -53,12 +55,14 @@ const LabelComponent = ({
     plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
     plane.isPickable = false;
 
-    // Funkce pro vyčištění
     return () => {
       plane.dispose();
-      dynamicTexture.dispose();
       material.dispose();
+      dynamicTexture.dispose();
     };
   }, [scene, positionX, positionZ, text, floorIndex, config]);
+
+  return null;
+};
 
 export default LabelComponent;
